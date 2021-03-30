74f503bb3c83eb4c5f8e162a521bbf08da44c372
ACLTearr <burroughskaelen@gmail.com>
2021-03-30 13:54:44 -0400

    Modified gem to rspec-rails, generated rails_helper, set base User test

---
+++ spec/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe User do
+    context 'testing' do
+        it 'tests' do
+            # user = User.new(email: nil)
+            # expect(user).to_not be_valid
+        end
+    end
+end
