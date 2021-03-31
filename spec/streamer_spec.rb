5a00b8f0d0c49637e52a0bd4d78d6ea06c4ec3ac
ACLTearr <burroughskaelen@gmail.com>
2021-03-31 11:59:21 -0400

    Finished Users tests, added intervention_spec

--- spec/streamer_spec.rb
+++ spec/streamer_spec.rb
@@ -1,21 +1,11 @@
 require 'elevator_media'
 
 #Weather for Montreal taken from https://weatherwidget.io/
-weather = '<div> <a class="weatherwidget-io" href="https://forecast7.com/en/45d50n73d57/montreal/" data-label_1="MONTREAL" data-label_2="WEATHER" data-theme="original" >MONTREAL WEATHER</a>
-<script>
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","weatherwidget-io-js");
-</script> </div>'
+weather = '<div> <a class="weatherwidget-io" href="https://forecast7.com/en/45d50n73d57/montreal/" data-label_1="MONTREAL" data-label_2="WEATHER" data-theme="original" >MONTREAL WEATHER</a> <script> !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","weatherwidget-io-js"); </script> </div>'
 
 describe ElevatorMedia::Streamer do
-    context 'test' do
-        it 'tests working so far' do
-            media = ElevatorMedia::Streamer.new
-            expect(media). to be_kind_of(ElevatorMedia::Streamer)
-        end
-    end
-    
     context 'Return weather' do
-        it 'Returns weather into a div' do
+        it 'should return weather for Montreal in a div' do
             expect(ElevatorMedia::Streamer.getContent()).to eq(weather)
         end
     end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,10 +1,35 @@
 require 'rails_helper'
 
 describe User do
-    context 'testing' do
-        it 'tests' do
-            # user = User.new(email: nil)
-            # expect(user).to_not be_valid
+
+    email = "test@test.com"
+    password = "123456"
+    newPassword = "654321"
+
+    user = User.create(email: email, password: password)
+
+    context 'Create User' do
+        it 'should create a new user' do
+
+            # Testing that email gets set and is valid
+            expect(user.email).to eq(email)
+            expect(user.email).to match(/\A\w+@\w+\.\w{2,3}\Z/)
+
+            # Testing that password gets set and is within length requirements
+            expect(user.password).to eq(password)
+            expect(user.password.length).to be_between(6, 20).inclusive
+
+        end
+    end
+
+    context 'Update User' do
+        it 'should change users password' do
+
+            user.update(password: newPassword)
+
+            # Testing that password updates
+            expect(user.password).to eq(newPassword)
+
         end
     end
 end
